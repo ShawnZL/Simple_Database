89df548b08f939c76c038833d05f3d1ad428ab43
Shawn Zhao <shawnzhao@ShawnMac.local>
2023-02-05 17:09:40 +0800

    20230205_2

--- main.c
+++ main.c
@@ -3,23 +3,32 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
 // build a sample REPL(read–execute–print loop)
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 #define TABLE_MAX_PAGES 100
 
+typedef struct {
+    int file_descripter;
+    uint32_t file_length;
+    void* pages[TABLE_MAX_PAGES];
+}Pager;
+
 // 指向页的数组指针
 typedef struct {
     uint32_t num_rows;
-    void* pages[TABLE_MAX_PAGES];
+    Pager* pager;
 } Table;
 
 // Row 代表一行数据，需要我们进行存储的数据
 typedef struct {
     uint32_t  id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1]; // string 以空字符结尾，所以需要加上这个！
+    char email[COLUMN_EMAIL_SIZE + 1];
 }Row;
 
 typedef struct {
@@ -28,6 +37,17 @@ typedef struct {
     ssize_t input_length; // 数据类型用来表示可以被执行读写操作的数据块的大小, signed
 } InputBuffer;
 
+typedef struct {
+    Table* table;
+    uint32_t row_num;
+    bool end_of_table; // Indicates a position one past the last element
+}Cursor;
+//the row is the way we can identify a location.
+
+typedef enum {
+    NODE_INTERNAL, NODE_LEAF
+}NodeType;
+
 typedef enum {
     META_COMMAND_SUCCESS,
     META_COMMAND_UNRECOGNIZED_COMMAND
@@ -35,8 +55,10 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
-    PREPARE_SYNTAX_ERROR,//参数数量不够
-    PREPARE_UNRECOGNIZED_STATEMENT
+    PREPARE_SYNTAX_ERROR,// 参数数量不够
+    PREPARE_NEGATIVE_ID, // 负数ID编号
+    PREPARE_UNRECOGNIZED_STATEMENT,
+    PREPARE_STRING_TOO_LONG // String超长
 } PrepareResult;
 
 typedef enum {
@@ -64,6 +86,35 @@ const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 const uint32_t PAGE_SIZE = 4096;
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+/*
++ * Common Node Header Layout
++ */
+const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
+const uint32_t NODE_TYPE_OFFSET = 0;
+const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
+const uint32_t IS_ROOT_OFFSET = NODE_TYPE_SIZE;
+const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
+const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+const uint8_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+
+/*
+ * Leaf Node Header Layout
+ */
+const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
+
+/*
++ * Leaf Node Body Layout
++ */
+const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_KEY_OFFSET = 0;
+const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
+const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
+const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
 // 新建初始化
 InputBuffer* new_input_buffer() {
@@ -101,42 +152,214 @@ void print_row(Row* row) {
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
+uint32_t* leaf_node_num_cells(void* node) {
+    return node + LEAF_NODE_NUM_CELLS_OFFSET;
+}
+
+uint32_t* leaf_node_cell(void* node, uint32_t cell_num) {
+    return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
+}
+
+uint32_t* leaf_node_key(void* node, uint32_t cell_num) {
+    return leaf_node_cell(node, cell_num);
+}
+
+void* leaf_node_value(void* node, uint32_t cell_num) {
+    return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
+}
+
+void initialize_leaf_node(void* node) { *leaf_node_num_cells(node) = 0; }
+
+Cursor* table_start(Table* table) {
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+
+    return cursor;
+}
+
+Cursor* table_end(Table* table) {
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+
+    return cursor;
+}
+
+// 从file中将page打开，恢复出来。
+Pager* pager_open(const char* filename) {
+    int fd = open(filename, O_RDWR|O_CREAT, S_IWUSR|S_IRUSR);// 第三个参数用来确定权限
+    // Read/Write mode,        Create file if it does not exist
+    // User write permission,  User read permission
+    if (fd == -1) {
+        printf("Unable to open file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    off_t file_length = lseek(fd, 0, SEEK_END); // lseek 的此用法是返回当前的偏移量
+    Pager* pager = malloc(sizeof(Pager));
+    pager->file_descripter = fd;
+    pager->file_length = file_length;
+
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; ++i) {
+        pager->pages[i] = NULL;
+    }
+    return pager;
+}
+
+// 新建page然后在内存中开辟空间，之后刷新到磁盘中
+void* get_page(Pager* pager, uint32_t page_num) {
+    if (page_num > TABLE_MAX_PAGES) {
+        printf("Tried to fetch page number out of bounds. %d > %d\n", page_num, TABLE_MAX_PAGES);
+        exit(EXIT_FAILURE);
+    }
+    // Cache miss. Allocate memory and load from file
+    if (pager->pages[page_num] == NULL) {
+        void* page = malloc(PAGE_SIZE);
+        uint32_t num_pages = pager->file_length / PAGE_SIZE;
+
+        // we might save a partial page at the end of the file
+        if (pager->file_length % PAGE_SIZE) {
+            num_pages += 1;
+        }
+        // 在中间插入
+        if (page_num <= num_pages) {
+            lseek(pager->file_descripter, page_num * PAGE_SIZE, SEEK_SET); //设置文件偏移量
+            ssize_t bytes_read = read(pager->file_descripter, page, PAGE_SIZE); //将文件转移进入page中，目前都在Cache中
+            if (bytes_read == -1) {
+                printf("Error reading file: %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        pager->pages[page_num] = page;
+    }
+    return pager->pages[page_num];
+}
+
+void pager_flush(Pager* pager, uint32_t page_num, uint32_t size) {
+    if (pager->pages[page_num] == NULL) {
+        printf("Tried to flush null page\n");
+        exit(EXIT_FAILURE);
+    }
+
+    off_t offset = lseek(pager->file_descripter, page_num * PAGE_SIZE, SEEK_SET);
+    // 设定偏移量，如果设置不成功，则返回错误
+    if (offset == -1) {
+        printf("Error seeking: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+    // 写入
+    ssize_t bytes_written = write(pager->file_descripter, pager->pages[page_num], size);
+    if (bytes_written == -1) {
+        printf("Error writing: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+}
+
 // initialize the table
-Table* new_table() {
+// new_table rename db_open
+//opening the database file, initializing a pager data structure, initializing a table data structure
+Table* db_open(const char* filename) {
+    Pager* pager = pager_open(filename);
+    uint32_t num_rows = pager->file_length / ROW_SIZE;
+    // 记录文件长度，然后除row大小可以得到总行数。
+
     Table* table = (Table*)malloc(sizeof(Table));
     table->num_rows = 0;
-    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
-        table->pages[i] = NULL;
-    }
+    table->pager = pager;
+    table->num_rows = num_rows;
     return table;
 }
 
-void free_table(Table* table) {
-    for (int i = 0; table->pages[i]; i++) {
-        free(table->pages[i]);
+//将Cache刷新进入磁盘中，关闭数据库 file，然后释放内存
+void db_close(Table* table) {
+    Pager* pager = table->pager;
+    uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;// 确定整数页
+
+    for (uint32_t i = 0; i < num_full_pages; ++i) {
+        if (pager->pages[i] == NULL) {
+            continue;
+        }
+        pager_flush(pager, i, PAGE_SIZE);
+        free(pager->pages[i]);
+        pager->pages[i] = NULL;
     }
+    // There may be a partial page to write to the end of the file
+    // This should not be needed after we switch to a B-tree
+    // 最后一页的东西
+    uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+    if (num_additional_rows > 0) {
+        uint32_t page_num = num_full_pages;
+        if (pager->pages[page_num] != NULL) { //结尾有page
+            pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+            free(pager->pages[page_num]);
+            pager->pages[page_num] = NULL;
+        }
+    }
+    int result = close(pager->file_descripter);
+    if (result == -1) {
+        printf("Error closing db file.\n");
+        exit(EXIT_FAILURE);
+    }
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        void* page = pager->pages[i];
+        if (page) {
+            free(page);
+            pager->pages[i] = NULL;
+        }
+    }
+    free(pager);
     free(table);
 }
 
+
 MetaCommandResult do_meta_command(InputBuffer* inputBuffer, Table* table) {
-    if (strcmp(inputBuffer->buffer, ".exit") == 0)
+    if (strcmp(inputBuffer->buffer, ".exit") == 0) {
+        db_close(table);
         exit(EXIT_SUCCESS);
+    }
     else
         return META_COMMAND_UNRECOGNIZED_COMMAND;
 }
 
+// 判断插入string是否超出界限
+PrepareResult prepare_Insert(InputBuffer* inputBuffer, Statement* statement) {
+    statement->type = STATEMENT_INSERT;
+    // strtok分割以谁结尾，第一个使用完毕后，结尾指针会存在strtok函数中
+    char* keyword = strtok(inputBuffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+
+    if (id_string == NULL || username == NULL || email == NULL)
+        return PREPARE_SYNTAX_ERROR;
+
+    int id = atoi(id_string);
+    if (id < 0) { // id 小于0
+        return PREPARE_NEGATIVE_ID;
+    }
+    if (strlen(username) > COLUMN_USERNAME_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username);
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS;
+}
+
 // 执行命令工作，insert和select两个东西需要进行存储
+// 但是这里前期有一个问题就是没有添加相应的判定str字节大小，string超出限制将无法进行存储，造成buffer溢出
 PrepareResult prepare_statement(InputBuffer* inputBuffer, Statement* statement) {
     // 把 str1 和 str2 进行比较，最多比较前 n 个字节。
     if (strncmp(inputBuffer->buffer, "insert", 6) == 0) {
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-                inputBuffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-                statement->row_to_insert.username, statement->row_to_insert.email);
-        if (args_assigned < 3) {
-            return PREPARE_SYNTAX_ERROR;// 参数数量不够
-        }
-        return PREPARE_SUCCESS;
+        return prepare_Insert(inputBuffer, statement);
     }
     if (strncmp(inputBuffer->buffer, "select", 6) == 0) {
         statement->type = STATEMENT_SELECT;
@@ -150,8 +373,9 @@ PrepareResult prepare_statement(InputBuffer* inputBuffer, Statement* statement) 
 // memcpy 从存储区 str2 复制 n 个字节到存储区 str1
 void serialize_row(Row* source, void* destination) {
     memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
-    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
-    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+
+    strncpy(destination + USERNAME_OFFSET, source->username, USERNAME_SIZE);
+    strncpy(destination + EMAIL_OFFSET, source->email, EMAIL_SIZE);
 }
 
 // 从destination复制文件进入source返回过去
@@ -161,19 +385,33 @@ void deserialize_row(void* source, Row* destination) {
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
-// 确定在哪里开始写页。目的是找到那一页。
+// old 确定在哪里开始写页。目的是找到那一页。
 // where to read/write in memory for a particular row
-void* row_slot(Table* table, uint32_t row_num) {
+/*void* row_slot(Table* table, uint32_t row_num) {
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = table->pages[page_num];
-    if (page == NULL) {
-        // Allocate memory only when we try to access page
-        page = table->pages[page_num] = malloc(PAGE_SIZE);
-    }
+    void* page = get_page(table->pager, page_num);
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}*/
+// new
+void* cursor_value(Cursor* cursor) {
+    uint32_t row_num = cursor->row_num;
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = get_page(cursor->table->pager, page_num);
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
     return page + byte_offset;
 }
+
+// cursor 移动
+void cursor_advance(Cursor* cursor) {
+    cursor->row_num += 1;
+    if (cursor->row_num >= cursor->table->num_rows) {
+        cursor->end_of_table = true;
+    }
+}
+
 
 // 执行插入
 ExecuteResult execute_insert(Statement* statement, Table* table) {
@@ -181,8 +419,9 @@ ExecuteResult execute_insert(Statement* statement, Table* table) {
         return EXECUTE_TABLE_FULL;
     }
     Row* row_to_insert = &(statement->row_to_insert);
+    Cursor* cursor = table_end(table);
     // table 是表，现有位置，然后根据table存储的num_rows计算应该插入到那一页
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows += 1;
     return EXECUTE_SUCCESS;
 }
@@ -190,11 +429,18 @@ ExecuteResult execute_insert(Statement* statement, Table* table) {
 // 执行选择
 ExecuteResult execute_select(Statement* statement, Table* table) {
     Row row;
-    for (uint32_t i = 0; i < table->num_rows; i++) {
+    Cursor* cursor = table_start(table);
+    /*for (uint32_t i = 0; i < table->num_rows; i++) {
         // 将table内容的复制到row中，然后展示出来
         deserialize_row(row_slot(table, i), &row);
         print_row(&row);
+    }*/
+    while (!(cursor->end_of_table)) {
+        deserialize_row(cursor_value(cursor), &row);
+        print_row(&row);
+        cursor_advance(cursor);
     }
+    free(cursor);
     return EXECUTE_SUCCESS;
 }
 
@@ -209,7 +455,14 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
 }
 
 int main(int argc, char* argv[]) {
-    Table* table = new_table();
+    if (argc < 2) {
+        printf("Must supply a database filename.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char* filename = argv[1];
+    Table* table = db_open(filename);
+
     InputBuffer* input_buffer = new_input_buffer();
     while (true) {
         print_prompt();
@@ -231,6 +484,12 @@ int main(int argc, char* argv[]) {
                 break;
             case (PREPARE_SYNTAX_ERROR):
                 printf("Syntax error. Could not parse statement.\n");
+                continue;
+            case (PREPARE_STRING_TOO_LONG):
+                printf("string is too long.\n");
+                continue;
+            case (PREPARE_NEGATIVE_ID):
+                printf("ID must be positive.\n");
                 continue;
             case (PREPARE_UNRECOGNIZED_STATEMENT):
                 printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
