1aec4f369d24c1875d6fc98700d94c97d9fc6541
Shawn Zhao <shawnzhao@ShawnMac.local>
2023-02-06 15:54:38 +0800

    20230206_1

--- main.c
+++ main.c
@@ -66,7 +66,8 @@ typedef enum {
 
 typedef enum {
     EXECUTE_SUCCESS,
-    EXECUTE_TABLE_FULL
+    EXECUTE_TABLE_FULL,
+    EXECUTE_DUPLICATE_KEY
 } ExecuteResult;
 
 typedef enum {
@@ -154,10 +155,11 @@ void print_row(Row* row) {
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
+//公共头部偏移量之后的东西，就是数量，返回数量指针
 uint32_t* leaf_node_num_cells(void* node) {
     return node + LEAF_NODE_NUM_CELLS_OFFSET;
 }
-
+// 叶节点头偏移+key*value之后偏移，需要添加位置的偏移量
 uint32_t* leaf_node_cell(void* node, uint32_t cell_num) {
     return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
 }
@@ -170,7 +172,22 @@ void* leaf_node_value(void* node, uint32_t cell_num) {
     return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
 }
 
-void initialize_leaf_node(void* node) { *leaf_node_num_cells(node) = 0; }
+// 得到node的类型，是root还是其他类型
+NodeType get_node_type(void* node) {
+    uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+    return (NodeType)value;// 类型转换
+}
+
+void set_node_type(void* node, NodeType type) {
+    uint8_t value = type;
+    *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
+}
+
+
+void initialize_leaf_node(void* node) {
+    set_node_type(node, NODE_LEAF);
+    *leaf_node_num_cells(node) = 0;
+}
 
 // 在pager中新建一个node(page)，给node开辟空间，之后刷新到磁盘中
 // page_num由调用给出，一般都用在Table->root_page_num。这样就是直接后插上
@@ -223,16 +240,52 @@ Cursor* table_start(Table* table) {
     return cursor;
 }
 
-Cursor* table_end(Table* table) {
+// 使用二分法进行查找
+Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key) {
+    void* node = get_page(table->pager, page_num);
+    uint32_t num_cells = *leaf_node_num_cells(node);
+
     Cursor* cursor = malloc(sizeof(Cursor));
     cursor->table = table;
-    cursor->page_num = table->root_page_num;
-    void* root_node = get_page(table->pager, table->root_page_num);
-    uint32_t num_cells = *leaf_node_num_cells(root_node);
-    cursor->cell_num = num_cells;
-    cursor->end_of_table = true;
+    cursor->page_num = page_num;
 
+    // Binary search
+    uint32_t min_index = 0;
+    uint32_t one_past_max_index = num_cells;
+    while (one_past_max_index != min_index) {
+        uint32_t index = (min_index + one_past_max_index) / 2;
+        uint32_t key_at_index = *leaf_node_key(node, index);
+        if (key == key_at_index) {
+            cursor->cell_num = index;
+            return cursor;
+        }
+        else if (key < key_at_index) {
+            one_past_max_index = index;
+        }
+        else {
+            min_index = index + 1;
+        }
+    }
+    cursor->cell_num = min_index;
     return cursor;
+}
+
+/*
+ Return the position of the given key.
+ If the key is not present, return the position
+ where it should be inserted
+*/
+Cursor* table_find(Table* table, uint32_t key) {
+    uint32_t root_page_num = table->root_page_num;
+    void* root_node = get_page(table->pager, root_page_num);
+
+    if (get_node_type(root_node) == NODE_LEAF) {
+        return leaf_node_find(table, root_page_num, key);
+    }
+    else {
+        printf("Need to implement searching an internal node\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 // 从file中将page打开，恢复出来。
@@ -322,13 +375,12 @@ void deserialize_row(void* source, Row* destination) {
     memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
-
 void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
     // 返回节点，没有的话新建
     void* node = get_page(cursor->table->pager, cursor->page_num);
     uint32_t num_cells = *leaf_node_num_cells(node);
     if (num_cells >= LEAF_NODE_MAX_CELLS) {
-        // Node full
+        // Node full 未实现Btree节点分开
         printf("Need to implement splitting a leaf node.\n");
         exit(EXIT_FAILURE);
     }
@@ -425,7 +477,7 @@ PrepareResult prepare_Insert(InputBuffer* inputBuffer, Statement* statement) {
 
     if (id_string == NULL || username == NULL || email == NULL)
         return PREPARE_SYNTAX_ERROR;
-
+    // 这没有判断id是否重复！
     int id = atoi(id_string);
     if (id < 0) { // id 小于0
         return PREPARE_NEGATIVE_ID;
@@ -488,12 +540,21 @@ void cursor_advance(Cursor* cursor) {
 // 执行插入
 ExecuteResult execute_insert(Statement* statement, Table* table) {
     void* node = get_page(table->pager, table->root_page_num);
-    if ((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS)) {
+    uint32_t num_cells = (*leaf_node_num_cells(node));
+    if (num_cells >= LEAF_NODE_MAX_CELLS) {
         return EXECUTE_TABLE_FULL;
     }
     Row* row_to_insert = &(statement->row_to_insert);
-    Cursor* cursor = table_end(table);
+    // Cursor* cursor = table_end(table); 不再需要插入到最后一页
     // table 是表，现有位置，然后根据table存储的num_rows计算应该插入到那一页
+    uint32_t key_to_insert = row_to_insert->id;
+    Cursor* cursor = table_find(table, key_to_insert);
+    if (cursor->cell_num < num_cells) {
+        uint32_t key_at_index = *leaf_node_key(node, cursor->cell_num);
+        if (key_at_index == key_to_insert) {
+            return EXECUTE_DUPLICATE_KEY;
+        }
+    }
     leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
     return EXECUTE_SUCCESS;
 }
@@ -575,6 +636,8 @@ int main(int argc, char* argv[]) {
             case (EXECUTE_TABLE_FULL):
                 printf("Error: Table full.\n");
                 break;
+            case (EXECUTE_DUPLICATE_KEY):
+                printf("Error: Duplicate key.\n");
         }
     }
     return 0;
