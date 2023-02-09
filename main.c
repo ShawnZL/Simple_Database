c955b1bc4f3adc99846f20538a3a613bca16bb1d
Shawn Zhao <shawnzhao@ShawnMac.local>
2023-02-09 21:09:25 +0800

    20230209_1

--- main.c
+++ main.c
@@ -572,37 +572,72 @@ void create_new_root(Table* table, uint32_t right_child_page_num) {
 // step1: create new internal and get the right old parent,
 // step2: create new internal(n+1) and join new child,
 // step3: link the new internal and the root
-void internal_node_split(Table* table, uint32_t parent_page_num, uint32_t child_page_num) {
-    /* 思考加入internal尚未更新的情况。
+
+void internal_node_split(Table* table, uint32_t need_split_page_num) {
+
+    void* old_node = get_page(table->pager, need_split_page_num);// old
+    uint32_t parent_page_num = *node_parent(old_node); // 确定现在internal的父母
     void* parent = get_page(table->pager, parent_page_num);
-    void* child = get_page(table->pager, child_page_num);
-    uint32_t child_max_key = get_node_max_key(child);
-    uint32_t index = internal_node_find_child(parent, child_max_key);
-    uint32_t right_child_page_num = *internal_node_right_child(parent);
-    void* right_child = get_page(table->pager, right_child_page_num);
-    uint32_t right_child_max_key = get_node_max_key(right_child);
-    */
-    void* old_node = get_page(table->pager, parent_page_num);// old
-    // 建立新的internal节点
-    uint32_t new_internal_page_num = get_unused_page_num(table->pager);
-    void* new_internal_node = get_page(table->pager, new_internal_page_num);
+
     // 建立新的right部分
     uint32_t new_page_num = get_unused_page_num(table->pager); // 获取新page页面。作为右半部分
     void* new_node = get_page(table->pager, new_page_num); // 建立新的page
-    // 找到目前internal里需要提升的key，也就是nums_keys / 2。
-    uint32_t old_node_num_keys = *internal_node_num_keys(old_node);
-    uint32_t med_key = old_node_num_keys / 2;
-    for (int32_t i = old_node_num_keys; i >= 0; i--) {
-        void* destination_node;
-        // 确定节点的在左孩子还是右孩子
-        if (i > med_key) {//大于左边计数
-            destination_node = new_node; // new_node代表右孩子，相对来说大的
-        } else {
-            destination_node = old_node; // old_node代表左孩子，相对来说小的
-        }
+    *node_parent(new_node) = *node_parent(old_node); //复制父亲
+    *leaf_node_next_leaf(new_node) = *leaf_node_next_leaf(old_node); // 新节点复制所有老节点的sibling
+    *leaf_node_next_leaf(old_node) = new_page_num; // old节点的sibling为新节点
 
+    // 找到目前internal里需要提升的key，也就是nums_keys / 2。
+    // 1 <= internal_node_num <= 2
+    uint32_t old_node_num_keys = *internal_node_num_keys(old_node);
+    uint32_t med_key = INTERNAL_NODE_MAX_CELLS / 2; // 1
+
+    /*
+     * 这里偷懒，使用保留 old 和 new，然后直接设置cell_num保证可以写入
+     * new中进行前移
+     * */
+    for (int32_t i = old_node_num_keys; i > med_key; i--) {
+        void* destination_node = new_node;
+
+        uint32_t index_within_node = i % (INTERNAL_NODE_MAX_CELLS - 1); // i % 2
+        void* destination = leaf_node_cell(destination_node, index_within_node);
+        memcpy(destination, leaf_node_cell(new_node, i), LEAF_NODE_CELL_SIZE);
     }
-    
+    *(internal_node_num_keys(old_node)) = med_key;
+    *(internal_node_num_keys(new_node)) = med_key;
+
+    // 其实根本不用思考提升谁，因为提升的这个东西是一个虚假的数据，只是一个数字，真正的索引还是放在叶子节点里
+    uint32_t original_num_keys = *internal_node_num_keys(parent);
+    uint32_t child_max_key = get_node_max_key(new_node);
+    uint32_t right_child_page_num = *internal_node_right_child(parent);
+    void* right_child = get_page(table->pager, right_child_page_num);
+
+    // internal_node_insert
+    if (child_max_key > get_node_max_key(right_child)) {
+        /* Replace right child */
+        *internal_node_child(parent, original_num_keys) = right_child_page_num;
+        *internal_node_key(parent, original_num_keys) =
+                get_node_max_key(right_child);
+        *internal_node_right_child(parent) = new_page_num;
+    } else {
+        /* Make room for the new cell */
+        for (uint32_t i = original_num_keys; i > index; i--) {
+            void* destination = internal_node_cell(parent, i);
+            void* source = internal_node_cell(parent, i - 1);
+            memcpy(destination, source, INTERNAL_NODE_CELL_SIZE);
+        }
+        *internal_node_child(parent, index) = new_page_num;
+        *internal_node_key(parent, index) = child_max_key;
+    }
+    //
+    if (is_node_root(old_node)) { //如果是root，则需要重新构建root
+        return create_new_root(table, new_page_num);
+    }
+    else {
+        if (original_num_keys >= INTERNAL_NODE_MAX_CELLS) {
+            internal_node_split(table, parent_page_num);
+            return;
+        }
+    }
 }
 
 void internal_node_insert(Table* table, uint32_t parent_page_num, uint32_t child_page_num) {
@@ -646,11 +681,11 @@ void internal_node_insert(Table* table, uint32_t parent_page_num, uint32_t child
 
     if (original_num_keys >= INTERNAL_NODE_MAX_CELLS) {
         // 需要添加分离split internal node
-        printf("Need to implement splitting internal node\n");
-        exit(EXIT_FAILURE);
-        /*
-        internal_node_split(table, parent_page_num, child_page_num);
-        return;*/
+        //printf("Need to implement splitting internal node\n");
+        //exit(EXIT_FAILURE);
+
+        internal_node_split(table, parent_page_num);
+        return;
     }
 }
 
